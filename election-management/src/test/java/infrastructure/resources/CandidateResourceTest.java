7d4b88a68d72dd195f59cc063c6deb219366dd29
Daniel Sobanski <dsobanski0@gmail.com>
2024-09-06 20:38:03 -0300

    Gerenciamento de Candidatos - API

--- election-management/src/test/java/infrastructure/resources/CandidateResourceTest.java
+++ election-management/src/test/java/infrastructure/resources/CandidateResourceTest.java
@@ -2,6 +2,7 @@ package infrastructure.resources;
 
 import api.CandidateApi;
 import api.dto.in.CreateCandidate;
+import api.dto.out.Candidate;
 import io.quarkus.test.InjectMock;
 import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
@@ -9,6 +10,8 @@ import jakarta.ws.rs.core.MediaType;
 import org.instancio.Instancio;
 import org.jboss.resteasy.reactive.RestResponse;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
 
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.*;
@@ -31,5 +34,19 @@ class CandidateResourceTest {
 
         verify(candidateApi).create(in);
         verifyNoMoreInteractions(candidateApi);
+    }
+
+    @Test
+    void list() {
+        var out = Instancio.stream(Candidate.class).limit(4).toList();
+
+        when(candidateApi.list()).thenReturn(out);
+
+        var response = given().when().get().then().statusCode(RestResponse.StatusCode.OK).extract().as(Candidate[].class);
+
+        verify(candidateApi).list();
+        verifyNoMoreInteractions(candidateApi);
+
+        assertEquals(out, Arrays.stream(response).toList());
     }
 }
