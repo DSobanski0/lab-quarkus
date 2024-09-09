2188e41f8f1849eaf100393b6f8722d86cf00ce6
Daniel Sobanski <dsobanski0@gmail.com>
2024-09-09 15:07:22 -0300

    Aplicação para votação

--- voting-app/src/main/java/infrastructure/lifecycle/Subscribe.java
+++ voting-app/src/main/java/infrastructure/lifecycle/Subscribe.java
@@ -1,9 +1,8 @@
 package infrastructure.lifecycle;
 
 import domain.Election;
-import infrastructure.lifecycle.repositories.RedisElectionRepository;
+import infrastructure.repositories.RedisElectionRepository;
 import io.quarkus.redis.datasource.ReactiveRedisDataSource;
-import io.quarkus.redis.datasource.RedisDataSource;
 import io.quarkus.runtime.Startup;
 import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.infrastructure.Infrastructure;
--- voting-app/src/main/java/infrastructure/repositories/RedisElectionRepository.java
+++ voting-app/src/main/java/infrastructure/repositories/RedisElectionRepository.java
@@ -1,9 +1,11 @@
-package infrastructure.lifecycle.repositories;
+package infrastructure.repositories;
 
 import domain.Candidate;
 import domain.Election;
 import domain.ElectionRepository;
+import io.quarkus.cache.CacheResult;
 import io.quarkus.redis.datasource.RedisDataSource;
+import io.quarkus.redis.datasource.keys.KeyCommands;
 import io.quarkus.redis.datasource.sortedset.SortedSetCommands;
 import jakarta.enterprise.context.ApplicationScoped;
 import org.jboss.logging.Logger;
@@ -17,16 +19,32 @@ public class RedisElectionRepository implements ElectionRepository {
 
     private final SortedSetCommands<String, String> sortedSetCommands;
 
+    private final KeyCommands<String> keyCommands;
+
     public RedisElectionRepository(RedisDataSource redisDataSource) {
         sortedSetCommands = redisDataSource.sortedSet(String.class, String.class);
+        keyCommands = redisDataSource.key(String.class);
     }
 
     @Override
+    @CacheResult(cacheName = "memoization")
     public Election findById(String id) {
         LOGGER.info("Retrieving election " + id + " from redis");
 
         List<Candidate> candidates = sortedSetCommands.zrange("election:" + id, 0, -1).stream().map(Candidate::new).toList();
 
         return new Election(id, candidates);
+    }
+
+    @Override
+    public List<Election> findAll() {
+        LOGGER.info("Retrieving elections from redis");
+        return keyCommands.keys("election:*").stream().map(id -> findById(id.replace("election:", ""))).toList();
+    }
+
+    @Override
+    public void vote(String electionId, Candidate candidate) {
+        LOGGER.info("voting for " + candidate.id());
+        sortedSetCommands.zincrby("election:" + electionId, 1, candidate.id());
     }
 }
