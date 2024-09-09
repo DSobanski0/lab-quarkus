ac4090c6f00bc2dccdc364a90efe01f06c10fc86
Daniel Sobanski <dsobanski0@gmail.com>
2024-09-09 11:40:20 -0300

    Gerenciamento de Candidatos - Publish

--- election-management/src/main/resources/db/migration/V2__CreateTableElections.sql
+++ election-management/src/main/resources/db/migration/V2__CreateTableElections.sql
@@ -1,12 +1,13 @@
-CREATE TABLE candidates (
+CREATE TABLE elections (
     id VARCHAR(40) NOT NULL,
-    photo VARCHAR(255) DEFAULT NULL,
-    given_name VARCHAR(50) NOT NULL,
-    family_name VARCHAR(50) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    phone VARCHAR(50) DEFAULT NULL,
-    job_title VARCHAR(50) DEFAULT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (id)
+);
+
+CREATE TABLE election_candidate (
+    election_id VARCHAR(40) NOT NULL,
+    candidate_id VARCHAR(40) NOT NULL,
+    votes INTEGER DEFAULT 0,
+    PRIMARY KEY (election_id, candidate_id)
 );
